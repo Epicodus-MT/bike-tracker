a66cbe03a2b648d3f5312c46d4d929fa42a12ded
Mara Timberlake <maratimberlake@msn.com>
2017-08-03 16:16:17 -0700

    edit API key

--- js/bike-tracker.js
+++ js/bike-tracker.js
@@ -1,11 +1,11 @@
-var apiKey = require('./../.env').apiKey;
+// var apiKey = require('./../.env').apiKey;
 
 function BikeTracker() {
 
 }
 
 BikeTracker.prototype.getBikes = function(location, getCount) {
-  $.get('https://bikeindex.org:443/api/v3/search/count?location='+location+'&distance=10&stolenness=stolen&access_token='+apiKey).then(function(response) {
+  $.get('https://bikeindex.org:443/api/v3/search/count?location='+location+'&distance=10&stolenness=stolen').then(function(response) {
       getCount(location, response.proximity);
       console.log(JSON.stringify(response));
     }).fail(function(error) {
@@ -13,18 +13,31 @@ BikeTracker.prototype.getBikes = function(location, getCount) {
     });
   };
 
-BikeTracker.prototype.getBikesByCity = function(city, getBikeStats) {
-  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=100&location='+city+'&distance=10&stolenness=proximity&access_token='+apiKey).then(function(response) {
+  BikeTracker.prototype.getBikesByCity = function(city, getBikeStats) {
+    $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=100&location='+city+'&distance=10&stolenness=proximity').then(function(response) {
 
-    response.bikes.forEach(function(bike) {
-      getBikeStats(city, bike);
-      console.log(bike);
+      response.bikes.forEach(function(bike) {
+        getBikeStats(city, bike);
+        console.log(bike);
+      });
+
+    }).fail(function(error) {
+      $('.city-results').text(error.responseJSON.message);
     });
+  };
 
-  }).fail(function(error) {
-    $('.city-results').text(error.responseJSON.message);
-  });
-};
+// BikeTracker.prototype.getBikesByCity = function(city, getBikeStats) {
+//   $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=100&location='+city+'&distance=10&stolenness=proximity&access_token='+apiKey).then(function(response) {
+//
+//     response.bikes.forEach(function(bike) {
+//       getBikeStats(city, bike);
+//       console.log(bike);
+//     });
+//
+//   }).fail(function(error) {
+//     $('.city-results').text(error.responseJSON.message);
+//   });
+// };
 
 
 exports.bikeModule = BikeTracker;
